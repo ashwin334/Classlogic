9a519f1fcf4bdca2ce4bd2c4a481c7c759e76ad3
ashwin333 <ashwinpatil832@gmail.com>
2022-10-09 17:08:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlogic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             int a = 10;
+            
+            int b = ++a;
+            int c = --b *  ++a;
+            int d = ++c *  ++b  + --a;
+            
+            Console.WriteLine(a); //11
+            Console.WriteLine(b); //11
+            Console.WriteLine(c);//121
+            Console.WriteLine(d);//1342
+                        
+        }
+    }
+}
